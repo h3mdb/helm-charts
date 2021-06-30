096c4983b6dcc7815e6d02e6184c9439d11fb836
h3mdb <85584907+h3mdb@users.noreply.github.com>
2021-06-29 19:04:39 -0600

    Update build.sh
    Point to Custom Fork

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 helm lint helm-chart-sources/*
 helm package helm-chart-sources/*
-helm repo index --url https://criblio.github.io/helm-charts/ .
+helm repo index --url https://github.com/h3mdb/helm-charts .
 
 
 git add *tgz index.yaml
